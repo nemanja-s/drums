319c68a72845d0595f6ae9cfff813d7cc51100a6
nemanja-s <33790061+nemanja-s@users.noreply.github.com>
2017-11-22 15:30:04 +0100

    Add files via upload

---
+++ js/drums.js
@@ -0,0 +1,12 @@
+
+function play(elid) {
+            var audio = document.getElementById(elid);
+            if (audio != null) {
+                audio.play();
+            }
+          }
+          function onKeyDown(event){
+            console.log(event);
+            play(event.key);
+          }
+window.addEventListener('keydown', onKeyDown);
